bc81bf4e27b4c4421bdb59cc4a1d2cb02e6b6a36
Silent-code1 <64768412+Silent-code1@users.noreply.github.com>
2023-02-15 08:00:13 +0100

    every single letters

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,27 +2,59 @@ import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import Loader from 'react-loaders'
 import LogoTitle from '../../assets/images/logo-s.png'
+import AnimatedLetters from '../AnimatedLetters'
 import './index.scss'
 
-
 const Home = () => {
-    
-    return (
-       <div className="container home-page">
-        <div className="text-zone">
-          <h1>Hi, <br />I'm
-          <img src={LogoTitle} alt="developer" />
-          olianik
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['o', 'l', 'i', 'a', 'n', 'i', 'k']
+  const jobArray = [
+    'w',
+    'e',
+    'b',
+    ' ',
+    'd',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
-          </h1>
-          <h2>Frontend developer / JavaScript / React</h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
-        </div>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend developer / JavaScript / React</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
-    ) 
+    </div>
+  )
 }
 
 export default Home
